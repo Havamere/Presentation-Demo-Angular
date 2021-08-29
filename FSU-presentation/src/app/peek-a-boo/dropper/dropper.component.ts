430627141e4e4bff9414ffc6727f7a4e6e9f1841
Kyle R <kyle.rinehart83@gmail.com>
2021-08-29 16:25:36 -0400

    Second commit. Forgot to remove calls to peek-a-boo content after removing associated files

--- FSU-presentation/src/app/peek-a-boo/dropper/dropper.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: 'app-dropper',
-  templateUrl: './dropper.component.html',
-  styleUrls: ['./dropper.component.css']
-})
-
-export class DropperComponent {}
